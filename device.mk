f0e1535558ddee7610dd6dc99e3d548348903ef1
TheMalachite <eliasgheeraert@gmail.com>
2021-05-19 18:39:21 +0200

    RMX2001: Import and redo device properties from stock
    - From RealmeUI V1.0 B.61

--- device.mk
+++ device.mk
@@ -170,9 +170,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Properties
-include $(LOCAL_PATH)/properties.mk
-
 # RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
