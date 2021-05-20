915078534a0f226f6f2b439ddbdfb0aa041b8a7f
TheMalachite <eliasgheeraert@gmail.com>
2021-05-20 23:06:33 +0200

    RMX2001: Import compatibility matrix from stock
    - From RealmeUI V1.0 B.61

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,7 @@ TARGET_NO_BOOTLOADER := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
